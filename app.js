94d74bc485f54f94c7f48e16a9d72f8ea7e60f32
LuChang <1256515509@qq.com>
2022-04-20 00:59:59 +0800

    delete usless packages

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Koa = require("koa")
 const app = new Koa()
 const bodyparser = require("koa-bodyparser")
 const logger = require("koa-logger")
-const static = require("koa-static")
 const db = require("./utils/database")
 
 const index = require("./routes/index")
@@ -11,13 +10,11 @@ const users = require("./routes/users")
 // middlewares
 app.use(bodyparser())
 app.use(logger())
-app.use(static(__dirname + "/public"))
 
-//静态服务，自动匹配index.html
-app.use(static(__dirname + "/dist"))
-
-//日志打印
+//测试日志打印
 app.use(async (ctx, next) => {
+  // console.log("req host :", ctx.request.host)
+  // console.log("req hostname :", ctx.request.hostname)
   console.log("req body :", ctx.request.body)
   console.log("req query :", JSON.stringify(ctx.query))
   await next()
